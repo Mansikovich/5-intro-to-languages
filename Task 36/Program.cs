1f810a04cb2507c5343444075e99b06b660641a8
Mansur Dagaev <mans.4e4en@mail.ru>
2023-02-03 13:10:42 +0300

    2

---
+++ Task 36/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+class Program
+{
+static void Main()
+{
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine($"Сгенерирован массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+    for (int z = 1; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
+
+    Console.WriteLine($"Сумма нечетных индексов {sum}");
+
+    void FillArrayRandomNumbers(int[] numbers)
+{
+    
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-10,10); 
+    }
+}
+    void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.WriteLine(numbers[i] + " ");
+    }
+   
+}
+
+}
+}
+}
